5c6426a0fdd754679f1309a033f34af164a6149c
Stefano Brozzi <stefano.brozzi@gmail.com>
2023-11-17 09:24:13 +0100

    variabili

---
+++ 013/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+variable "regione" {
+  default = "eu-south-1"
+  
+}
+
+
+provider "aws" {
+  region  = var.regione
+  profile = "sbrozzi@sinthera"
+}
+
+variable "my_list" {
+  default = ["Todd", "James", "Alice", "Dottie", "Alice"]
+}
+
+
+variable "environment" {
+  default = "dev"
+}
+
+resource "aws_iam_user" "the-accounts" {
+  for_each = var.environment == "prod" ? toset( ["Todd", "James", "Alice", "Dottie"] ) : toset([])
+      name     = each.key
+}
+
