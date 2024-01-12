b0e72fe6e9ef7e3e9287a3e600be58f02741c7db
Stefano Brozzi <stefano.brozzi@gmail.com>
2024-01-12 12:25:02 +0100

    last changes

---
+++ 041/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "aws" {
+  region  = "eu-south-1"
+  profile = "sbrozzi@sinthera"
+}
+
+resource "aws_instance" "example_server" {
+  ami           = "ami-025765768cef3dca5"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Terraform ${terraform.workspace}"
+  }
+}
+
+
+output "name" {
+  value = aws_instance.example_server.tags.Name
+  
+}
